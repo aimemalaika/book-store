8f9d212832df065361ceaef9182f45c7047cac49
Aime Malaika <aimemalaika1995@gmail.com>
2022-02-25 03:23:04 +0200

    Fix semantic text

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -4,7 +4,7 @@ import { removeBook } from '../redux/books/books';
 const BookList = () => {
   const books = useSelector((state) => state.booksReducer);
   const dispatch = useDispatch();
-  const removeBookToStore = (id) => {
+  const removeBookFromStore = (id) => {
     dispatch(removeBook(id));
   };
   return (
@@ -16,7 +16,7 @@ const BookList = () => {
           <li key={id}>
             <span>{title}</span>
             <span>{author}</span>
-            <button onClick={() => removeBookToStore(id)} type="button">Remove</button>
+            <button onClick={() => removeBookFromStore(id)} type="button">Remove</button>
           </li>
         ))}
       </ul>
